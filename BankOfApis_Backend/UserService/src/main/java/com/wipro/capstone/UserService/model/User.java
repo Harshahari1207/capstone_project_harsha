7e172f3301fa00817d648155c7d97548a512f650
Harshahari1207 <harsha733084@gmail.com>
2024-10-17 15:15:48 +0530

    Userservice all endpoints and functionality.

--- BankOfApis_Backend/UserService/src/main/java/com/wipro/capstone/UserService/model/User.java
+++ BankOfApis_Backend/UserService/src/main/java/com/wipro/capstone/UserService/model/User.java
@@ -1,5 +1,78 @@
 package com.wipro.capstone.UserService.model;
 
-public class User {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Getter
+@Setter
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	// Properties for the User class
+	private Long userId; // Primary key
+	private String username; // Username of the user
+	private String password; // Password for the user
+	private String address; // Address of the user
+
+	// Constructor
+	public User(Long userId, String username, String password, String address) {
+		this.userId = userId;
+		this.username = username;
+		this.password = password;
+		this.address = address;
+	}
+
+	// Default constructor
+	public User() {
+	}
+
+	// Getters and Setters
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	// toString method for displaying user information
+	@Override
+	public String toString() {
+		return "User{" + "userId=" + userId + ", username='" + username + '\'' + ", address='" + address + '\'' + '}';
+	}
 }
--- BankOfApis_Backend/UserService/src/main/java/com/wipro/capstone/UserService/repository/UserRepository.java
+++ BankOfApis_Backend/UserService/src/main/java/com/wipro/capstone/UserService/repository/UserRepository.java
@@ -1,5 +1,13 @@
 package com.wipro.capstone.UserService.repository;
 
-public class UserRepository {
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.wipro.capstone.UserService.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+	Optional<User> findByUsername(String username);
 }
