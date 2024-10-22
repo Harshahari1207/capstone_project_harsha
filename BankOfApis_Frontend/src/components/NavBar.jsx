ff496b989746d640ca209b870249d467efd19598
Harshahari1207 <harsha733084@gmail.com>
2024-10-22 10:04:57 +0530

    Handle logout.

--- BankOfApis_Frontend/src/components/NavBar.jsx
+++ BankOfApis_Frontend/src/components/NavBar.jsx
@@ -1,6 +1,12 @@
 import React from "react";
-
+import { useNavigate } from "react-router-dom";
 const NavBar = () => {
+
+  const history = useNavigate();
+  const handleLogout = () => {
+    localStorage.clear();
+    history("/");
+  }
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light w-100 d-flex justify-content-between align-items-center p-3">
       <a className="navbar-brand" href="/">
@@ -19,7 +25,7 @@ const NavBar = () => {
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarNav">
-        {localStorage.getItem("token") ? (
+        {localStorage.getItem("username") ? (
           <ul className="navbar-nav ms-auto">
             <li className="nav-item">
               <a className="nav-link" href="/">
@@ -27,7 +33,7 @@ const NavBar = () => {
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/">
+              <a className="nav-link"onClick={handleLogout}>
                 Logout
               </a>
             </li>
